8a4c56826c4d69f4e540f4c0cf5364e27fe1baad
Chandu Gowda <hackerbrogowda143@gmail.com>
2026-05-10 22:08:15 +0530

    Removed UnWanted Code

--- FrontEnd/src/router/ProtectedRoute.jsx
+++ FrontEnd/src/router/ProtectedRoute.jsx
@@ -27,22 +27,3 @@ const ProtectedRoute = () => {
 
 export default ProtectedRoute;
 
-// import { useContext } from "react";
-// import { GlobelValue } from "../context/GlobelVariable";
-// import { Outlet, Navigate } from "react-router-dom";
-
-// const ProtectedRoute = () => {
-//   const { JWT_Token } = useContext(GlobelValue);
-
-//   // If NOT logged in → redirect to login
-//   if (!JWT_Token) {
-//     return <Navigate to="/" replace />
-//   }
-
-//   // If logged in → allow access
-//   return <Outlet />;
-// };
-
-// export default ProtectedRoute;
-
-
